d99166728cdc12fe776b8e0fefaec778f27823e8
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-10-22 00:13:40 -0300

    feat: add the 'cidades', 'frases' and 'app' files

---
+++ app.py
@@ -0,0 +1,125 @@
+from cidades import listaCidades
+from frases import frasesProvocar
+from frases import frasesAproximar
+from random import *
+# Variáveis e funções iniciais
+pato = {}
+pato['droneCodigo'] = choice([
+    'abc123', 'def456', 'ghi789', 'jkl123', 'mno456', 'pqr789'
+])
+# Marca do drone
+# Fabricante do drone
+# País de origem do drone
+divisao = '============================================'
+cidade = 'DSIN City'
+droneSaude = 100
+escolha = 0
+def DetectarPatos():
+    global pato
+    def CriarPato():
+        # Aqui tudo que será personalizado de pato para pato e ataque para ataque
+        pato['status'] = choice(['desperto', 'em transe', 'em hibernação profunda'])
+        # Altura e peso do Pato
+        # Quantidade de mutações
+        # Se estiver desperto, Habilidade especial do Pato
+        # Se o país do drone for os EUA, converte de pés e libras para cm e g
+        # Coordenadas GPS.
+        # Lugar de referência se tiver
+        # Localização do pato = cidade atual - país da cidade + coordenadas GPS
+        # Precisão do drone. Se for dos EUA, converte de jardas para cm ou m
+
+    def ProvocarPato():
+        # Mensagem e efeitos de fazer isso
+        print(choice(frasesProvocar))
+        dano = randint(1, 100)
+        droneSaude -= dano
+        if droneSaude <= 0:
+            print("O pato destruiu seu drone...")
+            pass # em teoria fecha o programa, na prática...
+        print(f'Saúde do seu drone: {droneSaude}%')
+
+    
+    def AproximarPato():
+        # Coletar as Bmp
+        print(choice(frasesAproximar)) # pega uma aleatória
+        pato['bpm'] = randint(50, 200) # Número aleatório entre 0 e 100
+        print('Bpm coletadas')
+
+    def PatoEncontrado():
+        aproximar = ''
+        
+        while aproximar != 'S' and aproximar != 'N':
+            aproximar = str(input('Deseja se aproximar? [S/N]: '))
+        
+        if aproximar == 'S':
+            print(f"Drone se aproxima do pato que está {pato['status']}...")
+            if pato['status'] == 'desperto':
+                ProvocarPato()
+            else:
+                AproximarPato()
+
+    # Chance de 50% de ter um pato
+    # Se tiver, escolhe um pato aleatório da lista de patos
+    # Ativa a função para patos
+    temPato = choice([True, False])
+    
+    if temPato:
+        print("Alerta: Pato primordial encontrado!")
+        CriarPato()
+        print(f"Status: {pato['status']}")
+        PatoEncontrado()
+    else:
+        print('Não há patos aqui.')
+        print('Pense na cidade do seu próximo destino.')
+
+
+def EscolherCidade(listaCidades):
+    global cidade, escolha
+    escolha = 0
+
+    cidade1 = choice(listaCidades)
+    listaCidades.remove(cidade1)
+
+    cidade2 = choice(listaCidades)
+    listaCidades.remove(cidade2)
+    
+    cidade3 = choice(listaCidades)
+    listaCidades.remove(cidade3)
+    
+    cidade4 = choice(listaCidades)
+    listaCidades.remove(cidade4)
+
+    print(f'{divisao}\n>> Cidade atual: {cidade}')
+    
+    if cidade != 'DSIN City':
+        DetectarPatos()    
+    while escolha < 1 or escolha > 4:
+        print(f'[1] {cidade1}\n[2] {cidade2}\n[3] {cidade3}\n[4] {cidade4}')
+        
+        escolha = int(input('\nDigite seu destino (número inteiro de 1 a 4): '))
+        if escolha == -1:
+            break
+        if escolha < 1 or escolha > 4:
+            print('>> Você digitou um valor inválido, tente novamente.\n')
+
+    # Cadastrar a cidade corretamente
+    match escolha:
+        case 1:
+            cidade = cidade1
+        case 2:
+            cidade = cidade2
+        case 3:
+            cidade = cidade3
+        case 4:
+            cidade = cidade4
+
+# Programa rodando
+
+print(listaCidades)
+print('\nOlá soldado!')
+print(f'Esse é o seu drone: {pato["droneCodigo"]}')
+print('Sua missão é catalogar patos primordiais. Esse mundo está cheio deles, mas você tem que encontrá-los.')
+print('Escolha a cidade de destino:')
+
+while escolha != -1:
+    EscolherCidade(listaCidades)
---
+++ frases.py
@@ -0,0 +1,18 @@
+frasesAproximar = [
+    'Ele está no sonho dos anjos patos...',
+    'Ele está dormindo feito um patinho...',
+    'Ele está com tanto sono...',
+    'Ele está sonhando com sua casa...',
+    'Ele está até babando...',
+    'Ele está sonhando com Pata Swift...',
+    '[...] Mas nenhum patinho voltou de lá...',
+    'Tantas fez o moço, que foi para a panela...',
+]
+frasesProvocar = [
+    'Ele te olha com aqueles olhos azuis e...',
+    'Parece que você xingou a mulher dele...',
+    'O céu começa a ficar escuro...',
+    'Nossa! Você já reparou nos braços dele?...',
+    'Ele te olha e seu coração para...',
+    'Você disse que a mãe dele é quem?!...'
+]
